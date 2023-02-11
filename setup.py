c69d0e220c5d581f6f4d27d6dc2f371998fcfe97
Steve <vkhanhqui@gmail.com>
2023-02-11 15:54:22 +0700

    DOC: Update pypi document

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name="having_a_sip",
-    version="0.0.1",
-    description="Contain common Python functions which I used.",
+    version="0.0.2",
+    description="Contain common Python functions to help you have a sip instead of rebuilding them.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url="https://github.com/vkhanhqui/having-a-sip",
     author="Steve",
     author_email="vkhanhqui@gmail.com",
@@ -16,7 +18,7 @@ setup(
     python_requires=">=3.9",
     project_urls={
         "Documentation":
-            "https://github.com/vkhanhqui/having-a-sip",
+            "https://having-a-sip.readthedocs.io/en/latest",
         "Bug Reports":
             "https://github.com/vkhanhqui/having-a-sip/issues/new",
         "Source Code":
