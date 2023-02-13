efd4d01e398b181a41b3044776f6b058be3cddfe
Steve <vkhanhqui@gmail.com>
2023-02-13 16:10:57 +0700

    TEST: Add unit tests for having_a_sip.file_utils.create_dir function

--- tests/having_a_sip/test_file_utils.py
+++ tests/having_a_sip/test_file_utils.py
@@ -5,9 +5,40 @@ from src.having_a_sip import file_utils as _file_utils
 
 
 class FileUtilsTest(unittest.TestCase):
+    """Implement unit tests for functions in having_a_sip.file_utils"""
+
+    @patch("src.having_a_sip.file_utils.exists")
+    @patch("src.having_a_sip.file_utils.makedirs")
+    def test_create_nonexistent_dir(
+        self,
+        mock_make_dirs,
+        mock_exists
+    ):
+        """Test create_dir function. Create a nonexistent directory"""
+        # Mock data
+        mock_exists.return_value = False
+        # Test
+        _file_utils.create_dir("new_dir")
+        # Assert
+        mock_make_dirs.assert_called_once_with("new_dir")
+
+    @patch("src.having_a_sip.file_utils.exists")
+    @patch("src.having_a_sip.file_utils.makedirs")
+    def test_create_existent_dir(
+        self,
+        mock_make_dirs,
+        mock_exists
+    ):
+        """Test create_dir function. Create an existent directory"""
+        # Mock data
+        mock_exists.return_value = True
+        # Test
+        _file_utils.create_dir("new_dir")
+        # Assert
+        mock_make_dirs.assert_not_called()
 
     def test_read_txt_file(self):
-        """Test having_a_sip.file_utils.read_file function. Read a txt file"""
+        """Test read_file function. Read a txt file"""
         # Mock data
         file_content_mock = """Mock .txt file"""
         fake_file_path = "path/mock.txt"
@@ -23,7 +54,7 @@ class FileUtilsTest(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_read_json_file(self):
-        """Test having_a_sip.file_utils.read_file function. Read a json file"""
+        """Test read_file function. Read a json file"""
         # Mock data
         file_content_mock = """
         [{"test_key_1": "test_value_1", "test_key_2": "test_value_2"}]
